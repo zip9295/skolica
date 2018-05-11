02dd913f901e97ed33b1845493c9810b555d6064
YogiB34r <v.jankovic992@gmail.com>
2018-05-11 13:48:43 +0200

    user update

--- functions.php
+++ functions.php
@@ -1,29 +1,30 @@
 <?php
-     function validateRegisterForm($params){
-          if (!is_array($params)) {
-               throw new Exception('Given param is not an array');
-          }
-          if(isset($params['email']) and isset($params['password']) and isset($params['password-2'])) {
-               if(  (strlen($params['email']) > 6 and strlen($params['email'] <= 20) and strstr($params['email'], '@', true)) and
-               (strlen($params['password']) > 6 and strlen($params['password']) <= 14) and ($params['password-2'] === $params['password'])) {
-                    return true;
-               }
-               else {
-                    echo "nije ok";
-                    return false;
-               }
-          }
-          else {
-               return false;
-          }
-     }
-
-function validateUserForm($params){
+function validateRegisterForm($params)
+{
     if (!is_array($params)) {
         throw new Exception('Given param is not an array');
     }
-    if(isset($params['email']) and isset($params['password']) and isset($params['password-2']) and isset($params['firstName']) and isset($params['lastName']) and isset($params['username'])) {
-        if(
+    if (isset($params['email']) and isset($params['password']) and isset($params['password-2'])) {
+        if ((strlen($params['email']) > 6 and strlen($params['email'] <= 20) and strstr($params['email'], '@', true)) and
+            (strlen($params['password']) > 6 and strlen($params['password']) <= 14) and ($params['password-2'] === $params['password'])
+        ) {
+            return true;
+        } else {
+            echo "nije ok";
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+function validateUserForm($params)
+{
+    if (!is_array($params)) {
+        throw new Exception('Given param is not an array');
+    }
+    if (isset($params['email']) and isset($params['password']) and isset($params['password-2']) and isset($params['firstName']) and isset($params['lastName']) and isset($params['username'])) {
+        if (
             (strlen($params['email']) > 6 and strlen($params['email'] <= 20) and strstr($params['email'], '@', true)) and
             (strlen($params['password']) > 6 and strlen($params['password']) <= 14) and
             ($params['password-2'] === $params['password']) and
@@ -38,99 +39,223 @@ function validateUserForm($params){
             // (strlen($params['username']) > 2 and strlen($params['username']) < 32 and preg_match("/[^a-zA-Z0-9\_-]/i", $params['username']))
         ) {
             return true;
-        }
-        else {
+        } else {
             echo "nije ok";
             return false;
         }
-    }
-    else {
+    } else {
         return false;
     }
 }
-    function saveUser($params){
 
-        $fileName = saveImage();
-        $userData = [
-            'email' => $params['email'],
-            'password' => $params['password'],
-            'firstName' => $params['firstName'],
-            'lastName' => $params['lastName'],
-            'username' => $params['username'],
-            'image' => $fileName,
-            'status' => $params['status']
-        ];
-        $tmp = file_get_contents('storage.json');
-        if(strlen($tmp) === 0){
-            $data = [$userData];
-        }
-        else {
-            $data = json_decode($tmp);
-            $data[] = $userData;
-        }
-        return file_put_contents('storage.json',json_encode($data));
+function saveUser($params)
+{
+    $fileName = saveImage();
+    $userData = [
+        'email' => $params['email'],
+        'password' => createPasswordHash($params['password']),
+        'firstName' => $params['firstName'],
+        'lastName' => $params['lastName'],
+        'username' => $params['username'],
+        'image' => $fileName,
+        'status' => $params['status']
+    ];
+    $tmp = file_get_contents('storage.json');
+    if (strlen($tmp) === 0) {
+        $data = [$userData];
+    } else {
+        $data = json_decode($tmp);
+        $data[] = $userData;
     }
-        function saveImage(){
-            $fileName = APP_PATH . '/images/' . $_FILES['image']['name'];
-            if(!move_uploaded_file($_FILES['image']['tmp_name'], $fileName)){
-                echo "Nismo snimili sliku";
-                die();
-            }
-            return 'images/'.$_FILES['image']['name'];
+    return file_put_contents('storage.json', json_encode($data));
+}
+
+function saveImage()
+{
+    $fileName = APP_PATH . '/images/' . $_FILES['image']['name'];
+    if (!move_uploaded_file($_FILES['image']['tmp_name'], $fileName)) {
+        echo "Nismo snimili sliku";
+        die();
+    }
+    return 'images/' . $_FILES['image']['name'];
+}
+
+function registerUser($params)
+{
+    $data = file_get_contents('storage.json');
+    $data .= json_encode(['email' => $params['email'], 'password' => $params['password']]) . PHP_EOL;
+    file_put_contents('storage.json', $data);
+}
+
+function getUserByEmail($email)
+{
+    foreach (getUsers() as $user) {
+        if ($email === $user->email) {
+            return $user;
+        }
+    }
+
+    return false;
+}
+
+function login($email, $password)
+{
+    $user = getUserByEmail($email);
+    if (!$user) {
+        return false;
+    }
+    if ($user->password === createPasswordHash($password)) {
+        $_SESSION['isLoggedIn'] = true;
+        return true;
+    }
+    return false;
+}
+
+function validateLoginForm($params)
+{
+    if (!is_array($params)) {
+        throw new Exception('Given param is not an array');
+    }
+    if (isset($params['email']) and isset($params['password'])) {
+        if ((strlen($params['email']) > 6 and strlen($params['email'] <= 20) and strstr($params['email'], '@', true)) and
+            (strlen($params['password']) > 6 and strlen($params['password']) <= 14)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+function bootstrap()
+{
+    define('APP_PATH', __DIR__);
+    session_start();
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
+
+/*
+ * Get users from file storage
+ * Return array
+ */
+function getUsers()
+{
+    $users = file_get_contents('storage.json');
+    return json_decode($users);
+}
+
+function isLoggedIn()
+{
+    if (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] === true) {
+        return true;
+    }
+
+    return false;
+}
+
+function redirect($baseUrl, $route = '', $statusCode = 302)
+{
+    header('location:' . $baseUrl . $route, $statusCode);
+}
+
+function logOut()
+{
+    unset($_SESSION);
+    session_destroy();
+}
+
+function createPasswordHash($password)
+{
+    return md5($password);
+}
+
+function validateArticleForm()
+{
+    if (isset($_POST['body']) && strlen($_POST['body']) < 1 || isset($_POST['category']) && strlen($_POST['category']) < 1 || isset($_POST['user']) && strlen($_POST['user']) < 1) {
+        return false;
+    }
+    return true;
+
+}
+
+function saveArticleForm($params)
+{
+    //$fileName = saveImage();
+    $articleData = [
+        'title' => $params['title'],
+        'description' => $params['description'],
+        'body' => $params['body'],
+        'category' => $params['category'],
+        'user' => $params['user'],
+        //'image' => $fileName,
+    ];
+    $tmp = file_get_contents('article.json');
+    if (strlen($tmp) === 0) {
+        $data = [$articleData];
+    } else {
+        $data = json_decode($tmp);
+        $data[] = $articleData;
+    }
+    return file_put_contents('article.json', json_encode($data));
+}
+
+function getArticleByTitle($title)
+{
+    foreach (getArticles() as $article) {
+        if ($title === $article->title) {
+            return $article;
+        }
+    }
+
+    return false;
+}
+
+function getArticles()
+{
+    $articles = file_get_contents('article.json');
+    return json_decode($articles);
+}
+
+function saveArticle($params)
+{
+//    $articleData = [
+//        'title' => $params['title']
+//    ];
+
+    $articles = [];
+    foreach (getArticles() as $article) {
+        if ($article->title === trim($params['title'])) {
+            $articles[] = $params;
+        } else {
+            $articles[] = $params;
         }
 
-     function registerUser($params){
-          $data = file_get_contents('storage.json');
-          $data .= json_encode(['email' => $params['email'], 'password' => $params['password'] ]) . PHP_EOL;
-          file_put_contents('storage.json', $data);
-     }
+    }
+    return file_put_contents('article.json', json_encode($articles));
+}
 
-     function getUserByEmail($email){
-          foreach (getUsers() as $user) {
-               if($email === $user->email){
-                    return $user;
-               }
-          }
+function saveCategoryForm($params)
+{
+    $userData = [
+        'category' => $params['category'],
+    ];
+    $tmp = file_get_contents('category.json');
+    if (strlen($tmp) === 0) {
+        $data = [$userData];
+    } else {
+        $data = json_decode($tmp);
+        $data[] = $userData;
+    }
+    return file_put_contents('category.json', json_encode($data));
+}
 
-          return false;
-     }
-     function login($email, $password){
-          $user = getUserByEmail($email);
-          if(!$user){
-               return false;
-          }
-          if($password === $user->password){
-              $_SESSION['isLoggedIn'] = true;
-              return true;
-          }
-          return false;
-     }
+function getCategory()
+{
+    $categories = file_get_contents('category.json');
+    return json_decode($categories);
+}
 
-     function bootstrap(){
-          session_start();
-          error_reporting(E_ALL);
-          ini_set('display_errors', 1);
-     }
-     /*
-      * Get users from file storage
-      * Return array
-      */
-     function getUsers () {
-         $users = file_get_contents('storage.json');
-         return json_decode($users);
-     }
-     function isLoggedIn () {
-          if (isset($_SESSION['isLoggedIn'] ) && $_SESSION['isLoggedIn'] === true) {
-               return true;
-          }
-
-          return false;
-     }
-     function redirect ($baseUrl, $route='', $statusCode= 302) {
-          header ('location:'. $baseUrl. $route, $statusCode) ;
-     }
-     function logOut () {
-          unset($_SESSION);
-          session_destroy();
-     }
- ?>
+?>
--- route.php
+++ route.php
@@ -13,70 +13,119 @@
                     break;
 
                 case 'login':
-                    if(validateLoginForm($_POST) and login($_POST['email'], $_POST['password'])) {
-                         redirect($config['baseUrl'],'userList&message=loggedIn') ;
+                    if (validateLoginForm($_POST) and login($_POST['email'], $_POST['password'])) {
+                        redirect($config['baseUrl'], '&message=loggedIn');
 
-                    }else {
-                         echo "Nisu dobri parametri";
-                         include('loginForm.phtml');
+                    } else {
+                        echo "Nisu dobri parametri";
+                        include('loginForm.phtml');
                     }
                     break;
 
                 case 'registerForm':
-                    include('registerForm.phtml');
+                    include('userForm.phtml');
                     break;
 
                 case 'register':
 
-                        break;
+                    break;
                 case 'userList':
                     if (isset($_GET['message']) && $_GET['message'] === 'logedIn') {
                         $message = 'Uspesno ste se ulogovali';
                     }
                     $users = array();
                     $emailFilter = null;
-                    if (isset($_GET['emailFilter'])){
+                    if (isset($_GET['emailFilter'])) {
                         array_push($users, getUserByEmail($_GET['emailFilter']));
-                    }else {
-                        $users = getUsers ();
+                    } else {
+                        $users = getUsers();
                     }
                     include 'userList.phtml';
                     break;
                 case 'userCreate':
                     $valid = validateUserForm($_POST);
-                    if(!$valid){
+                    if (!$valid) {
                         global $message;
                         $message = $valid;
-                    }
-                    else {
+                    } else {
                         if (!saveUser($_POST)) {
                             $message = 'Doslo je do greske prilikom snimanja korisnika';
-                        }
-                        else {
+                        } else {
                             echo "Korisnik je uspesno sacuvan";
                         }
                     }
                     break;
                 case 'userCreateForm':
-                    include ('userForm.phtml') ;
+                    include('userForm.phtml');
                     break;
                 case 'userUpdate':
 
+
                     break;
                 case 'userUpdateForm':
-                    $user= getUserByEmail($_GET['email']);
-                    include ('userForm.phtml');
+                    $user = getUserByEmail($_GET['email']);
+                    include('userForm.phtml');
 
                     break;
                 case 'userLogout' :
-                    logOut() ;
+                    logOut();
                     redirect($config['baseUrl']);
 
                     break;
+                case 'articleCreateForm' :
+                    $categories= array();
+                    $categories= getCategory();
+                    include('articleForm.phtml');
 
-                default:
-                         echo "default";
                     break;
-          }
+                case 'articleCreate' :
+                    if (!saveArticleForm($_POST)) {
+                        $message = 'Doslo je do greske prilikom snimanja clanka';
+                    } else {
+                        echo "Clanak uspesno sacuvan";
+
+                    }
+                    break;
+
+                case 'articleUpdateForm' :
+                    $categories= array();
+                    $categories= getCategory();
+                    $article = getArticleByTitle($_GET['title']);
+                    include 'articleForm.phtml';
+                    break;
+                case 'articleUpdate' :
+                        saveArticle($_POST);
+                    break;
+                case 'articleList' :
+                    $articles = array();
+                    $articleFilter = null;
+                    if (isset($_GET['articleFilter'])) {
+                        array_push($article, getArticleByTitle($_GET['articleFilter']));
+                    } else {
+                        $articles = getArticles();
+                    }
+                    include 'articleList.phtml';
+                    break;
+                case 'categoryCreateForm' :
+                    $categories = array();
+                    $categories = getCategory();
+                    include 'categoryCreate.phtml';
+                    break;
+                case 'categorySave' :
+                    if (!saveCategoryForm($_POST)) {
+                        $message = 'Doslo je do greske prilikom snimanja';
+                    } else {
+                        echo "Kategorija je uspesno snimljena u bazu";
+                        $categories = array();
+                        $categories = getCategory();
+                        include 'categoryCreate.phtml';
+                    }
+                    break;
+                default:
+                    echo "default";
+                    break;
+            }
+
+
      }
  ?>
