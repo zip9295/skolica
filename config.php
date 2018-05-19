79b9452776a7ae8cc4aefd6149cd6243b196e4e2
Yogi <v.jankovic992@gmail.com>
2018-05-19 20:18:56 +0200

    config sredjen

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 return [
-    'baseUrl'=>'http://127.0.0.1/edsa-Kurs/php/appDemo/index.php?route='
+    'baseUrl'=>'http://127.0.0.1/edsa-Kurs/php/appDemo/index.php?route=',
+//    'baseUrl'=>'http://127.0.0.1/edsa-kurs/index.php?route='
 
 
 ];
